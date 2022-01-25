06fa501e8e7802881fea30b95a3749d63fe2c09a
hyperkarmic <randy_pan@yahoo.com>
2022-01-25 18:39:24 +0000

    nothing much here

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+console.log(true, false)
+
+let email = `luigi@thenetninja.co.uk`
+let names = ['mario', 'peach', 'toad']
+let result = email.includes('@')
+
+let age = 25
+
+let name = 'shaun'
+
+console.log(age !== 30)
